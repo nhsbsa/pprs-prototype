9c2c08094b40d8c39159d8fb910fba7dadceb680
JAHMO <janssen.howitson-morley@nhs.net>
2023-10-13 12:54:44 +0100

    fixed clickable card focus to cover the whole card

--- index.js
+++ index.js
@@ -220,9 +220,12 @@ app.get('/pcp-prototype-version-2/new-request/current-requests', (req, res) => {
 app.get('/pcp-prototype-version-2/new-request/file-upload', (req, res) => { res.render('pcp-prototype-version-2/new-request/file-upload') });
 app.get('/pcp-prototype-version-2/new-request/file-upload1', (req, res) => { res.render('pcp-prototype-version-2/new-request/file-upload1') });
 app.get('/pcp-prototype-version-2/new-request/file-upload2', (req, res) => { res.render('pcp-prototype-version-2/new-request/file-upload2') });
+app.get('/pcp-prototype-version-2/auto-email/choose-main-suppliers', (req, res) => { res.render('pcp-prototype-version-2/auto-email/choose-main-suppliers') });
+app.get('/pcp-prototype-version-2/auto-email/card-1', (req, res) => { res.render('pcp-prototype-version-2/auto-email/card-1') });
+app.get('/pcp-prototype-version-2/auto-email/confirm-page', (req, res) => { res.render('pcp-prototype-version-2/auto-email/confirm-page') });
+app.get('/pcp-prototype-version-2/auto-email/card-2', (req, res) => { res.render('pcp-prototype-version-2/auto-email/card-2') });
 
 // PCP prototype version 3
-
 app.get('/pcp-prototype-version-3/homepage', (req, res) => { res.render('pcp-prototype-version-3/homepage') });
 app.get('/pcp-prototype-version-3/quarterly-multiple-screens', (req, res) => { res.render('quarterly-multiple-screens') });
 app.get('/pcp-prototype-version-3/quarterly-expander-list', (req, res) => { res.render('quarterly-expander-list') })
