9fb7176640daf527328031d0b75c9e5da108c4f5
Tom Briggs <thomas.briggs1@nhs.net>
2023-11-09 16:42:35 +0000

    user account area pages

--- index.js
+++ index.js
@@ -229,6 +229,16 @@ app.get('/pcp-prototype-version-2/auto-email/card-2', (req, res) => { res.render
 
 app.get('/pcp-prototype-version-3/choose-option', (req, res) => { res.render('pcp-prototype-version-3/choose-option') });
 app.get('/pcp-prototype-version-3/homepage', (req, res) => { res.render('pcp-prototype-version-3/homepage') });
+
+app.get('/pcp-prototype-version-3/login', (req, res) => { res.render('pcp-prototype-version-3/login') });
+app.get('/pcp-prototype-version-3/account-area', (req, res) => { res.render('pcp-prototype-version-3/account-area') });
+app.get('/pcp-prototype-version-3/log-out', (req, res) => { res.render('pcp-prototype-version-3/log-out') });
+app.get('/pcp-prototype-version-3/new-user/forename', (req, res) => { res.render('pcp-prototype-version-3/new-user/forename') });
+app.get('/pcp-prototype-version-3/new-user/surname', (req, res) => { res.render('pcp-prototype-version-3/new-user/surname') });
+app.get('/pcp-prototype-version-3/new-user/email', (req, res) => { res.render('pcp-prototype-version-3/new-user/email') });
+app.get('/pcp-prototype-version-3/new-user/user-type', (req, res) => { res.render('pcp-prototype-version-3/new-user/user-type') });
+app.get('/pcp-prototype-version-3/new-user/confirm', (req, res) => { res.render('pcp-prototype-version-3/new-user/confirm') });
+
 app.get('/pcp-prototype-version-3/homepage-dh', (req, res) => { res.render('pcp-prototype-version-3/homepage-dh') });
 app.get('/pcp-prototype-version-3/quarterly-multiple-screens', (req, res) => { res.render('quarterly-multiple-screens') });
 app.get('/pcp-prototype-version-3/quarterly-expander-list', (req, res) => { res.render('quarterly-expander-list') })
